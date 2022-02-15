bf46a6766eaa57ca8ab5a052f6969cb121e58bbd
Anastasiya <nastya.petrova1996@gmail.com>
2022-02-15 17:17:50 +0300

    Condition-commit

--- tasks/condition/condition.js
+++ tasks/condition/condition.js
@@ -6,10 +6,26 @@
  * Task 1 - if else - z1 : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
  * Task 2 - ternary operator - z2: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
  */
-let z1;
-let z2;
+
 const x = 3;
 const y = 7;
+let z1;
+let z2;
+if (y < 10) {
+    z1 = x + y;
+    console.log (z1);
+} else {
+    z1 = x * y;
+    console.log (z1)
+}; 
+z2 = (y < 10 ) ? x + y : x * y;
+
+console.log(z2); 
+
+/**let z1;
+let z2;
+const x = 3;
+const y = 7;*/
 
 
 /** Task: 2
@@ -19,10 +35,42 @@ const y = 7;
  * Assign result to task2 variable
  * Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
  */
-let task2;
+ let task2; 
+ const alpha = 3;
+ const beta = 7;
+ const operator = "add";
+ switch (operator) {
+    case "add":
+        task2 = alpha + beta; 
+        console.log (task2);
+        break;
+    case "subtract":
+        task2 = alpha - beta;
+        console.log (task2);
+        break;
+    case "multiply":
+        task2 = alpha * beta;
+        console.log (task2);
+        break;
+    case "divide":
+        task2 = alpha / beta; 
+        console.log (task2);
+        break;
+    default:
+        console.log ("error");
+        break;
+ }; 
+            
+
+
+
+
+
+
+/**let task2;
 const alpha = 3;
 const beta = 7;
-const operator = "add";
+const operator = "add"; /*
 
 
 /**
@@ -36,9 +84,29 @@ const operator = "add";
  * - 2701 - 6500: 45
  * assign the result to the 'discount' variable
  */
-const redemption = 420;
-let discount;
 
+let discount;
+const redemption = 420;
+if (redemption >= 0 && redemption <= 350) {
+    discount = 0;
+    console.log ("0%");
+} else if (redemption >= 351 && redemption<= 1350){
+    discount = 15;
+    console.log ("15%");
+} else if (redemption >= 1351 && redemption <= 2700) {
+    discount = 30;
+    console.log ("30%");
+
+} else if (redemption >= 2701 && redemption <= 6500) {
+    discount = 45;
+    console.log ("45%");
+} else {
+    console.log ("error")
+}; 
+
+
+/**const redemption = 420;
+let discount; */ 
 
 module.exports = {
     z1,
