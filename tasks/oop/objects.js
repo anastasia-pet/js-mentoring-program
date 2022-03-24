915c9a99b0c7a5382ecb76258be749379b96e1c7
Anastasiya <nastya.petrova1996@gmail.com>
2022-03-24 16:57:46 +0300

    objects-commit

--- tasks/oop/objects.js
+++ tasks/oop/objects.js
@@ -3,18 +3,41 @@
  * create object person with firstName and lastName properties
  * implement method getFullName that returns full name
  */
-const person = ''; //put you object here
+
+const person = {
+	firstName: "john",
+	lastName: "dou",
+	getFullName() {
+		 return this.firstName + " " + this.lastName
+	}
+}; 
+
+
 
 /**
  * create object student that has property grade and method getGrade
  * set person as prototype of student
  */
-const student = ''; //put you object here
+
+const student = {
+	firstName: "Name",
+	lastName: "LastName",
+	grade: 1,
+	getGrade() {
+		return this.grade
+	},
+	getFullName: function() {
+		return this.firstName + " " + this.lastName
+   }
+};
+Object.setPrototypeOf(person, student)
+
 
 /**
  * create new instance of student using Object.create
  */
-const student2 = ''; //put you object here
+ 
+ const student2 = Object.create(student);
 
 module.exports = {
 	person,
